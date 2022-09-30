7f7c2dca512eeceee2b7503fd450c04ac0c72251
Brant <brantb15@gmail.com>
2022-09-30 16:20:52 -0400

    :sparkles: New Theme Index
    Inserted generic index.php file

--- themes/AutoTrader/index.php
+++ themes/AutoTrader/index.php
@@ -1,17 +1,41 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+<?php wp_head(); ?>
+</head>
+<body>
+<h1><?php bloginfo( 'name' ); ?></h1>
+<h2><?php bloginfo( 'description' ); ?></h2>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+<?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+		<h3><?php the_title(); ?></h3>
+
+		<?php the_content(); ?>
+		<?php wp_link_pages(); ?>
+		<?php edit_post_link(); ?>
+
+		<?php endwhile; ?>
+
+	<?php
+    if ( get_next_posts_link() ) {
+        next_posts_link();
+    }
+    ?>
+	<?php
+    if ( get_previous_posts_link() ) {
+        previous_posts_link();
+    }
+    ?>
+
+	<?php else: ?>
+
+<p>No posts found. :(</p>
+
+<?php endif; ?>
+<?php wp_footer(); ?>
+</body>
+</html>
